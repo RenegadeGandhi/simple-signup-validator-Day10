8392a18ea033e0351ff3acc3eba9ab0f0ce147b4
Renegade Gandhi <osinayaoludare@gmail.com>
2020-05-10 15:02:31 +0100

    A simple signup validator

---
+++ signupValidator.js
@@ -0,0 +1,26 @@
+function validator (email, password) {
+
+    //Regular expression checks email value for "any@any.com" format.
+    let emailCheck = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,4})+$/;
+
+    /*Regular expression checks for uppercase, lowercase, number,
+    non-alphanumeric character and sets a minimum length.*/
+    let passwordCheck = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[^a-zA-Z0-9])(?!.*\s).{8,12}$/; 
+
+    /*test() searches the input string for the patterns defined
+    by the emailCheck and passwordCheck regular expressions and
+    the if statement returns the appropriate object*/
+    if (emailCheck.test(email) && passwordCheck.test(password)) {
+        return {
+            email: true,
+            password: true,
+        };
+    } else {
+        return {
+            email: false,
+            password: false,
+        };
+    }
+}
+
+validator("jud@gl.com", "H1@hhhhh")
